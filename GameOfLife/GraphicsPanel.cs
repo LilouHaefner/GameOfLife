ee4ca6ac27c74e34ba64680f5473abd2d0d37b2a
Lilou <lhaefnerpersonal@gmail.com>
2021-07-20 16:22:03 -0400

    Add graphics panel

---
+++ GameOfLife/GraphicsPanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    class GraphicsPanel : Panel
+    {
+        public GraphicsPanel()
+        {
+            // enable double buffering
+            this.DoubleBuffered = true;
+
+            // force repaint on window resize
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+    }
+}
